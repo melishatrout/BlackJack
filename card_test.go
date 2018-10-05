1239edf2eeec8cf96c60da0da1706bdae5d5816e
mdt764 <mdt764@cs.bham.ac.uk>
2018-10-05 01:09:13 +0100

    Adding new files

---
+++ card_test.go
@@ -0,0 +1,85 @@
+package blackjack
+
+import (
+	"fmt"
+	"testing"
+)
+
+func ExampleCard(){
+	fmt.Println(Card{Rank: Ace, Suit: Heart})
+	fmt.Println(Card{Rank: Two, Suit: Spade})
+	fmt.Println(Card{Rank: Nine, Suit: Diamond})
+	fmt.Println(Card{Rank: Jack, Suit: Club})
+	fmt.Println(Card{Suit: Joker})
+
+
+	//Output:
+	//Ace of Hearts
+	//Two of Spades
+	//Nine of Diamonds
+	//Jack of Clubs
+	//Joker
+}
+
+func TestNew(t *testing.T) {
+	cards := New()
+	// 13 ranks * 4 suits
+
+	if len(cards) != 13*4 {
+		t.Error("Wrong number of cards in the new deck")
+	}
+}
+
+func TestDefaultSort(t *testing.T) {
+	cards := New(DefaultSort)
+	exp := Card{Rank: Ace, Suit: Spade}
+	if cards[0] != Card(exp){
+		t.Error("Expected Ace of Spades as first card. Received:", cards[0])
+
+	}
+}
+
+func TestSort(t *testing.T) {
+	cards := New(Sort(Less))
+	exp := Card{Rank: Ace, Suit: Spade}
+	if cards[0] != Card(exp){
+		t.Error("Expected Ace of Spades as first card. Received:", cards[0])
+
+	}
+
+}
+
+func TestJokers(t *testing.T) {
+	cards := New(Jokers(4))
+	count := 0
+
+	for _, c := range cards {
+		if c.Suit == Joker {
+			count ++
+		}
+	}
+	if count != 4 {
+		t.Error("Expected 4 Jokers, received:", count)
+	}
+}
+
+func TestFilter(t *testing.T) {
+	filter := func(card Card) bool {
+		return card.Rank == Two || card.Rank == Three
+	}
+	cards := New(Filter(filter))
+	for _, c := range cards {
+		if c.Rank == Two || c.Rank == Three {
+			t.Error("Expected all Two's and Three's to be filtered out")
+		}
+	}
+}
+
+func TestDeck(t *testing.T) {
+	cards := New(Deck(3))
+	i := len(cards)
+	if i != 13*4*3 {
+		t.Errorf("Expected %d cards, received %d cards. ", 13*4*3, len(cards) )
+	}
+
+}
